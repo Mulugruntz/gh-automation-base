353821eefa12bc6d001caafb9fbad296800c8a2a
Samuel GIFFARD <samuel@giffard.co>
2023-08-27 13:53:48 +0200

    First automation (quotes) with PostgreSQL (#1)
    Add GitHub Action run-task, update `README.md`
    Add example pipeline `Quotes`.

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # gh-automation-base
-A project base for automating Python tasks using GitHub actions and Google tools
+A project base for automating Python tasks using GitHub actions and an Aiven for PostgreSQL database.
+
+## Setup
+1. Create a new repository using this repository as a template.
+2. Create a new Aiven PostgreSQL service.
+3. Copy `.env.example` to `.env` and fill in the values.
+4. Install poetry and the dependencies (base + dev) using the following commands:
+
+    ```bash
+    pip install poetry
+    poetry install
+    poetry install -E dev
+    ```
+
+5. Initialize the database using the following command:
+
+    ```bash
+    make migration-init migration-apply
+    ```
+
+6. Push to GitHub, set the [repo secrets][github-secrets] and enable GitHub actions.
+7. This will start populating your database with Quotes (example pipeline).
+
+
+[github-secrets]: https://docs.github.com/en/actions/reference/encrypted-secrets
--- gh_automation_base/cli.py
+++ gh_automation_base/cli.py
@@ -1,15 +1,59 @@
 """Console script for GitHub Automation Base."""
 
 import typer
+import asyncpg
+import asyncio
 
+from gh_automation_base.config import Config
+from gh_automation_base.persistence.init import cmd_init, cmd_clean
+from gh_automation_base.pipelines.quotes import cmd_quotes
 
 app = typer.Typer()
 
 
+async def time_async() -> None:
+    """Get the time from the DB and prints it."""
+    config = Config()
+    connection = await asyncpg.connect(config.postgres.dsn)
+    try:
+        result = await connection.fetch("SELECT now();")
+        typer.echo(result[0][0])
+    finally:
+        await connection.close()
+
+
 @app.command()
-def hello(name: str) -> None:
-    """Say hello to NAME"""
-    typer.echo(f"Hello {name}")
+def time() -> None:
+    """Show the time in the DB."""
+    asyncio.run(time_async())
+
+
+@app.command()
+def dsn() -> None:
+    """Print the DSN."""
+    config = Config()
+    typer.echo(config.postgres.dsn)
+
+
+@app.command()
+def init() -> None:
+    """Initialize a new project"""
+    config = Config()
+    asyncio.run(cmd_init(config))
+
+
+@app.command()
+def clean() -> None:
+    """Clean the database"""
+    config = Config()
+    asyncio.run(cmd_clean(config))
+
+
+@app.command()
+def quotes() -> None:
+    """Run the quotes pipeline"""
+    config = Config()
+    asyncio.run(cmd_quotes(config))
 
 
 def main() -> None:
